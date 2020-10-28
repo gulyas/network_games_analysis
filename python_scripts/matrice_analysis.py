2ab2e8f8249e4f533ded8af3a552324300973933
Tamás Csertán <33261144+csertant@users.noreply.github.com>
2020-10-28 17:38:34 +0100

    Improve scripts
    Former-commit-id: d361379ba317f6061a387b8fe296d9809b9bf481

--- python_scripts/matrice_analysis.py
+++ python_scripts/matrice_analysis.py
@@ -12,17 +12,21 @@ SAVE_PATH = "D:\\network_games\\matrice\\"
 
 def read_graph(filename):
     """Reads whole graph from file."""
-    return igraph.load(filename)
+    graph = igraph.load(filename)
+    print(f'Loaded {filename}.')
+    return graph
 
 
 def load_data(filename):
     """Loads data from Firebase export."""
     with open(filename, "r") as json_file:
         data = json.load(json_file)
+        print(f'Loaded {filename}.')
         return data['games']
 
 
 def analyse_data(data, graph):
+    players_stats = []
     for player, player_games in data.items():
         print("Analysing {}'s data...".format(player))
         player_stats = {
@@ -39,6 +43,7 @@ def analyse_data(data, graph):
             start = game['startState']
             end = game['endState']
             shortest = graph.shortest_paths_dijkstra(source=start, target=end, weights=None)
+            shortest = shortest[0][0]
             player_stats['shortest_clicks'].append(shortest)
             chain = game['stateChain'].split(" ")
             del chain[-1]
@@ -46,31 +51,35 @@ def analyse_data(data, graph):
             for i, state in enumerate(chain):
                 if i == 0:
                     continue
-                e = player_graph.es.find(source=chain[i - 1], target=state)
-                e['weight'] += 1
+                try:
+                    e = player_graph.es.find(_source=int(chain[i - 1]), _target=int(state))
+                    e['weight'] += 1
+                except ValueError:
+                    print("Not existing edge found.")
 
         # Plotting graph
-        colors = ["orange", "darkorange", "red", "blue"]
+        colors = ["grey", "orange", "red", "blue"]
         for e in player_graph.es:
             weight = e['weight']
             if weight >= 15:
                 e['color'] = colors[3]
             elif 8 <= weight < 15:
                 e['color'] = colors[2]
-            elif 3 <= weight < 8:
+            elif 2 <= weight < 8:
                 e['color'] = colors[1]
             else:
                 e['color'] = colors[0]
 
         visual_style = {"bbox": (3000, 3000), "margin": 17, "vertex_color": 'grey', "vertex_size": 20,
-                        "vertex_label_size": 8, "edge_curved": False, "layout": player_graph.layout("kk")}
+                        "vertex_label_size": 8, "edge_curved": False, "layout": player_graph.layout("fr")}
 
         save_name = f'matrice_{player}.eps'
         igraph.plot(player_graph, SAVE_PATH + save_name, **visual_style)
         print("Graph from {} analysed and plotted to {}".format(player, save_name))
         # Saving results
-        with open(SAVE_PATH + 'matrice_results.json', 'a') as fp:
-            json.dump(player_stats, fp, indent=4)
+        players_stats.append(player_stats)
+        with open(SAVE_PATH + 'matrice_results.json', 'w') as fp:
+            json.dump(players_stats, fp, indent=2)
 
 
 def main():
--- python_scripts/user_graph_analysis_mysql.py
+++ python_scripts/user_graph_analysis_mysql.py
@@ -12,7 +12,7 @@ PATH = "D:\\network_games\\"
 SAVE_PATH = "D:\\network_games\\scaffold\\"
 FILENAME = "scaffold_data_mysql.csv"
 
-USER = ""
+USER = "nbobbed37"
 
 
 def parse_data(filename):
@@ -28,19 +28,19 @@ def parse_data(filename):
         line_count = 0
         user_count = 0
 
+        user_last_clicks = {}
+        user_graph = igraph.Graph()
+
         for row in csv_reader:
             if line_count == 0:
                 print(f'Columns: {", ".join(row)}')
                 line_count += 1
-            else:
+            elif row[2] == USER:
                 line_count += 1
                 user = row[2]
                 article = row[3]
                 game = row[4]
-                if user != USER:
-                    continue
 
-                user_graph = igraph.Graph()
                 # Add edge to the users graph
                 try:
                     user_graph.vs.find(article)
@@ -56,6 +56,8 @@ def parse_data(filename):
                             user_graph.add_edge(source=user_last_clicks['article'], target=article, weight=1)
 
                 user_last_clicks = {"article": article, "game": game}
+            else:
+                continue
 
     print("{} users created".format(user_count))
     return user_graph, user
@@ -74,18 +76,21 @@ def analyse_graphs(user_graph, user):
 
     fig, ax = plt.subplots()
     ax.plot(x, counts, 'bo')
-    ax.set_x_label("Weights")
-    ax.set_y_label("Occurrences")
-    ax.set_title("Edge weights distribution")
+    ax.set_xlabel("Weights (Number of uses)")
+    ax.set_ylabel("Occurrences (Number of edges with particular weight)")
+    ax.set_title("Edge weight distribution")
+    plt.yscale("log")
+    plt.xscale("log")
+    plt.grid()
     fig.savefig(SAVE_PATH + f"mysql_{user}_ew.png")
-    plt.show()
-
+    # plt.show()
 
     # Creating subgraph by betweenness centrality
     btwn = user_graph.betweenness(directed=True, weights=None)
-    ntile = np.percentile(btwn, 20)
+    ntile = np.percentile(btwn, 90)
     sub_vs = user_graph.vs.select([v for v, b in enumerate(btwn) if b >= ntile])
     sub_graph = user_graph.subgraph(sub_vs)
+    print(f'Generated subgraph with {sub_graph.vcount()} vertices and {sub_graph.ecount()} edges.')
 
     colors = ["orange", "darkorange", "red", "blue"]
     for e in sub_graph.es:
@@ -99,14 +104,14 @@ def analyse_graphs(user_graph, user):
         else:
             e['color'] = colors[0]
 
-    visual_style = {"bbox": (3000, 3000), "margin": 17, "vertex_color": 'grey', "vertex_size": 20,
-                    "vertex_label_size": 8, "edge_curved": False}
+    visual_style = {"bbox": (3000, 3000), "margin": 17, "vertex_color": 'grey', "vertex_size": 10,
+                    "vertex_label_size": 4, "edge_curved": False}
 
     # Set the layout
     try:
-        layout = sub_graph.layout("kk")
+        layout = sub_graph.layout("fr")
         visual_style["layout"] = layout
-        save_name = f'mysql_reduced_{user}.eps'
+        save_name = f'mysql_{user}_reduced.eps'
         igraph.plot(sub_graph, SAVE_PATH + save_name, **visual_style)
         print("Graph from {} analysed and plotted to {}".format(user, save_name))
     except MemoryError:
